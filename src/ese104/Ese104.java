2708a3389e90a07a8112f8530bb63e81e7efef1d
Eracli Caterina <eracli.caterina@gmail.com>
2015-04-17 22:43:29 +0200

    Aggiunto main (da completare)

--- src/ese104/Ese104.java
+++ src/ese104/Ese104.java
@@ -1,10 +1,16 @@
 package ese104;
+import agenziaviaggi.*;
 
 public class Ese104 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	public static void main(String[] args) throws Exception {
+		
+		Cliente cliente = new Cliente("Mario", "Rossi");
+		Filiale filiale = new Filiale(1);
+		Pacchetto pacchetto = new Pacchetto("P57", "Weekend a Parigi", 3);
+		Prenotazione prenotazione = new Prenotazione("Pr341", 20, cliente, filiale);
+		
+		pacchetto.addPrenotazione(prenotazione);	
+		}
 }
