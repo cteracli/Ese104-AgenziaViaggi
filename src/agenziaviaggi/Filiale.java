8074f015fc5c8b1d510a4fbf8dc7dfb5e21fa7ae
Eracli Caterina <eracli.caterina@gmail.com>
2015-04-17 22:14:01 +0200

    da fare getPostiDisponibili e correggere errore riga21 in Pacchetto)

---
+++ src/agenziaviaggi/Filiale.java
@@ -0,0 +1,21 @@
+package agenziaviaggi;
+
+public class Filiale {
+	
+	private int numero;
+	
+	public Filiale(int numero){
+		this.numero=numero;
+	}
+	
+	public int getNumero(){
+		return this.numero;
+	}
+
+	@Override
+	public String toString(){
+		return "Filiale [numero=" + numero + "]";
+	}
+	
+
+}
---
+++ src/agenziaviaggi/Pacchetto.java
@@ -0,0 +1,51 @@
+package agenziaviaggi;
+
+import java.util.Vector;
+
+public class Pacchetto {
+	public String codice;
+	public String descrizione;
+	public int postiTotali;
+	public Vector<Prenotazione> prenotazione;
+	
+	public  Pacchetto( String codice, String descrizione, int postiTotali){
+		this.codice=codice;
+		this.descrizione=descrizione;
+		this.postiTotali=postiTotali;
+		this.prenotazione=new Vector<Prenotazione>();
+	}
+	
+	public void addPrenotazione(Prenotazione p) throws Exception{
+		if(this.getPostiDisponibili() > 0 ){
+			prenotazione.addElement(p);
+			p.setPacchetto(this);
+		}else{
+			throw new Exception("Non vi sono più posti per questo pacchetto");
+		}
+	}
+	
+	public int getPostiDisponibili(){
+		
+	}
+	
+	public Vector<Prenotazione> getPrenotazione(){
+		return prenotazione;
+	}
+	
+	public String getDescrizione(){
+		return descrizione;
+	}
+	
+	public int getPostiTotali(){
+		return postiTotali;
+	}
+	
+	public String getCodice(){
+		return codice;
+	}
+	@Override
+	public String toString() {
+		return "Pacchetto [codice=" + codice + ", descrizione=" + descrizione
+				+ ", postiTotali=" + postiTotali + ", prenotazioni="+ prenotazione + "]";
+	}
+}
