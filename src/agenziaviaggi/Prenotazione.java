5ed136e798f5387287600a3f76018cb0231c0487
Eracli Caterina <eracli.caterina@gmail.com>
2015-04-17 22:49:01 +0200

    Non aveva aggiunto Prenotazione

--- src/agenziaviaggi/Prenotazione.java
+++ src/agenziaviaggi/Prenotazione.java
@@ -1,5 +1,46 @@
 package agenziaviaggi;
 
 public class Prenotazione {
-
+	private String codice;
+	private int postiRichiesti;
+	private Cliente cliente;
+	private Filiale filiale;
+	private Pacchetto pacchetto;
+	
+	
+	public Prenotazione( String codice, int postiRichiesti, Cliente c, Filiale f){
+		this.codice=codice;
+		this.postiRichiesti=postiRichiesti;
+		this.cliente = c;
+		this.filiale = f;
+	}
+	
+	public void setPacchetto(Pacchetto p){
+		this.pacchetto=p;
+	}
+	
+	public String getCodice(){
+		return this.codice;
+	}
+	
+	public int getPostiRichiesti(){
+		return this.postiRichiesti;
+	}
+	
+	public Cliente getCliente(){
+		return this.cliente;
+	}
+	
+	public Filiale getFiliale(){
+		return this.filiale;
+	}
+	
+	@Override
+	public String toString() {
+		return "Prenotazione [codice=" + codice + ", postiRichiesti="
+				+ postiRichiesti + ", cliente=" + cliente + ", filiale="
+				+ filiale + ", Pacchetto [codice=" + pacchetto.getCodice() + ", descrizione=" + pacchetto.getDescrizione()
+				+ ", postiTotali=" + pacchetto.getPostiTotali() + ", prenotazioni="
+				+ "]";
+	}
 }
